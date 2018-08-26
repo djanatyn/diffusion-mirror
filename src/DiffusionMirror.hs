bdb1c66fa75339d63e3225c41fc7fc719af9ba19
Jonathan Strickland <djanatyn@gmail.com>
2018-08-26 19:06:39 -0400

    separate API response and JSON object

--- src/DiffusionMirror.hs
+++ src/DiffusionMirror.hs
@@ -18,27 +18,50 @@ data Config = Config
   { token  :: Text
   , apiURL :: Text } deriving (Generic, Show)
 
-data Repo = Repo
-  { phid :: Text 
-  , id   :: Text } deriving Show
-
 instance FromJSON Config
+
+data Repo = Repo
+  { repoPHID :: Text
+  , repoID   :: Int } deriving Show
+
+data URI = URI
+  { uriID   :: Int
+  , uriPHID :: Text
+  , uriRepo :: Text
+  , uriURL  :: Text } deriving Show
+
+data RepositoryEdit = RepositoryEdit Repo deriving Show
+
+instance FromJSON RepositoryEdit where
+  parseJSON = withObject "response" $ \o -> do
+    repo <- o .: "result" >>= (.: "object")
+
+    return (RepositoryEdit repo)
 
 instance FromJSON Repo where
   parseJSON = withObject "response" $ \o -> do
-    phid <- o .: "result" >>= (.: "object") >>= (.: "phid")
-    id   <- o .: "result" >>= (.: "object") >>= (.: "id")
+    repoPHID <- o .: "phid"
+    repoID   <- o .: "id"
 
     return Repo{..}
+
+instance FromJSON URI where
+    parseJSON = withObject "uri" $ \o -> do
+      uriID   <- o .: "id"
+      uriPHID <- o .: "phid"
+      uriURL  <- o .: "fields" >>= (.: "uri") >>= (.: "effective")
+      uriRepo <- o .: "fields" >>= (.: "repositoryPHID")
+
+      return URI{..}
 
 loadConfig :: FilePath -> IO Config
 loadConfig path = do
     contents <- B.readFile path
     maybe (error "could not decode") return $ decode contents
 
-createRepository :: Text -> Config -> IO Repo
+createRepository :: Text -> Config -> IO RepositoryEdit
 createRepository repo config = do
-  response <- asJSON =<< getWith opts url 
+  response <- asJSON =<< getWith opts url
   return $ response ^. responseBody where
     opts = defaults & param "transactions[0][type]"  .~ ["vcs"]
                     & param "transactions[0][value]" .~ ["git"]
@@ -46,6 +69,15 @@ createRepository repo config = do
                     & param "transactions[1][value]" .~ [repo]
                     & param "api.token" .~ [token config]
     url = unpack $ apiURL config
+
+-- getURIs :: [Repo] -> Config -> IO (Response (Map String Value))
+-- getURIs phids config = do
+--   response <- asJSON =<< getWith opts url
+--   return $ response ^. responseBody where
+--     url  = unpack $ apiURL config
+--     opts = defaults & param "constraints[phids]" .~ [show $ fmap unpack phids]
+--                     & param "attachments[uris]"  .~ ["true"]
+--                     & param "api.token"          .~ [token config]
 
 main :: IO ()
 main = do
